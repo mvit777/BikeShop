490e76e425b4aa681f75f59865b2e2d1cd681521
Seasoned Apprentice <7marvit@gmail.com>
2021-10-12 12:25:06 +0200

    added components library

---
+++ BikeShop.BlazorComponents/Components/HtmlTable.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeShop.BlazorComponents.Components
+{
+    public partial class HtmlTable<TItem>
+    {
+        [Parameter]
+        public RenderFragment HeaderTemplate { get; set; }
+
+        [Parameter]
+        public RenderFragment<TItem> RowTemplate { get; set; }
+
+        [Parameter]
+        public RenderFragment FooterTemplate { get; set; }
+
+        [Parameter]
+        public IReadOnlyList<TItem> Items { get; set; }
+    }
+}
