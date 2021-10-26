c786dac479499eb0045faee934441d2acd9f2f17
Seasoned Apprentice <7marvit@gmail.com>
2021-10-26 10:19:27 +0200

    grpc mapping completed

--- BikeShopWS/Services/BikeService.cs
+++ BikeShopWS/Services/BikeService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
 using BikeDistributor.Domain.Entities;
+using BikeDistributor.Domain.Models;
+using BikeDistributor.Infrastructure.interfaces;
 using BikeDistributor.Infrastructure.services;
 using BikeShopWS.Infrastructure;
 using BikeShopWS.Protos;
@@ -20,12 +22,20 @@ namespace GrpcBike
         private MongoServiceInstanceRegister _register;
         private readonly MongoBikeService _bikeService;
         private WsConfig _config;
+        private IMapper _mapper;
         public BikesService(ILogger<BikesService> logger, MongoServiceInstanceRegister register,WsConfig config)
         {
             _logger = logger;
             _register = register;
             _config = config;
-            _bikeService = (MongoBikeService)_register.GetServiceInstance("MongoBikeService", _config.GetMongoServiceIdentity("MongoBikeService")); 
+            _bikeService = (MongoBikeService)_register.GetServiceInstance("MongoBikeService", _config.GetMongoServiceIdentity("MongoBikeService"));
+            var mapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<MongoEntityBike, EntityMongoBike>();
+                cfg.CreateMap<BikeOption, EntityBikeOption>();
+                cfg.CreateMap<IBike, EntityBike>();
+            });
+            _mapper = mapperConfiguration.CreateMapper();
         }
 
         public override Task<BikesHelloReply> SayHello(BikesHelloRequest request, ServerCallContext context)
@@ -38,16 +48,12 @@ namespace GrpcBike
 
         public override async Task<GetBikesResponse> GetBikes(Empty request, ServerCallContext context)
         {
-            var configuration = new MapperConfiguration(cfg =>
-            {
-                cfg.CreateMap<MongoEntityBike, EntityBike>();
-            });
-            var mapper = configuration.CreateMapper();
+            
             List<MongoEntityBike> mebs = await _bikeService.Get();
             var response = new GetBikesResponse();
             foreach (MongoEntityBike meb in mebs)
             {
-                var eb = mapper.Map<EntityBike>(meb);
+                var eb = _mapper.Map<EntityMongoBike>(meb);
                 Console.WriteLine(eb.Id);
                 response.BikeEntities.Add(eb);
             }
