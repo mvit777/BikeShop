abe9dac8d91476084d54a0e0e4b58a13986e23e8
Seasoned Apprentice <7marvit@gmail.com>
2021-11-10 09:26:20 +0100

    added Autofade prop to Alert Component

--- BikeShop.BlazorComponents/Components/Alert.cs
+++ BikeShop.BlazorComponents/Components/Alert.cs
@@ -3,7 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace BikeShop.BlazorComponents.Components
 {
@@ -14,17 +16,35 @@ namespace BikeShop.BlazorComponents.Components
         [Parameter]
         public virtual string HTMLCssClass { get; set; } = "alert-primary";
         [Parameter]
+        public virtual double AutoFade { get; set; } = 0;
+        [Parameter]
         public virtual RenderFragment ChildContent { get; set; }
         [Parameter]
         public virtual bool Visible { get; set; } = false;
+
+        //see https://wellsb.com/csharp/aspnet/blazor-timer-navigate-programmatically/
+        private System.Timers.Timer _timer;
         public void ChangeVisible(bool visible, bool executeStateHasChanged = false)
         {
             Visible = visible;
-            //if (executeStateHasChanged)
-            //{
-            //    StateHasChanged();
-            //}
+            if (visible)
+            {
+                if(AutoFade > 0)
+                {
+                    _timer = new System.Timers.Timer(AutoFade);
+                    _timer.Elapsed += NotifyTimerElapsed;
+                    _timer.Enabled = true;
+                }
+            }
             
+        }
+        public event Action OnElapsed;
+        private void NotifyTimerElapsed(Object source, ElapsedEventArgs e)
+        {
+            OnElapsed?.Invoke();
+            Visible = false;
+            _timer.Dispose();
+            StateHasChanged();
         }
         public void ChangeCssClass(string cssClass, bool executeStateHasChanged = false)
         {
@@ -33,6 +53,10 @@ namespace BikeShop.BlazorComponents.Components
             //{
             //    StateHasChanged();
             //}
+        }
+        public void SetAutoFade(double autofade)
+        {
+            AutoFade = autofade;
         }
     }
 }
