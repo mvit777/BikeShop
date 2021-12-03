707e7dc77d68cb5fcd8fd66cc927717480b4de33
Seasoned Apprentice <7marvit@gmail.com>
2021-12-03 18:13:50 +0100

    updated appsettings-dist

--- BikeShop/Services/ConfigService.cs
+++ BikeShop/Services/ConfigService.cs
@@ -1,5 +1,6 @@
 ﻿using BikeDistributor.Infrastructure.core;
 using Newtonsoft.Json.Linq;
+using System.Collections;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -10,13 +11,14 @@ namespace BikeShop.Services
         private HttpClient _client;
         private string _jsonFilePath;
         private string _rawJson;
+       
         public ConfigService(string jsonFilePath, HttpClient client)
         {
             _client = client;
             _jsonFilePath = jsonFilePath;
             
         }
-
+        
         public async Task LoadAsync()
         {
             using var response = await _client.GetAsync(_jsonFilePath);
@@ -35,7 +37,6 @@ namespace BikeShop.Services
             string[] u = urlName.Split(new char[] { '.' });
             JToken o = JObject.Parse(_rawJson)["BikeShopWS"]["urls"];
             JToken url = o.SelectToken(u[0]).Value<string>(u[1]);
-            //var url = o["BikeShopWS"]["urls"][0][u[0]][urlName].ToString();
             
             return url.ToString();
         }
